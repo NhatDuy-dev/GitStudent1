2077f083b1d39655de6506dc696907ff50267677
NhatDuy-dev <thainhatduy2k5@gmail.com>
2025-04-22 02:33:26 +0700

    add updateName

--- src/main/java/com/mycompany/gitstudent/GitStudent.java
+++ src/main/java/com/mycompany/gitstudent/GitStudent.java
@@ -1,24 +1,39 @@
-
-
 package com.mycompany.gitstudent;
-
 
 public class GitStudent {
 
     public class Student {
-    private String studentId;
-    private String name;
-    private int age;
+// Các thuộc tính
+        private String studentId;
+        private String name;
+        private int age;
+// Constructor khởi tạo đối tượng Student
+        public Student(String studentId, String name, int age) {
+            this.studentId = studentId;
+            this.name = name;
+            this.age = age;
+        }
+//lấy thông tin sinh viên
+        public String getStudentId() {
+            return studentId;
+        }
 
-    public Student(String studentId, String name, int age) {
-        this.studentId = studentId;
-        this.name = name;
-        this.age = age;
+        public String getName() {
+            return name;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
+        // Phương thức cập nhật tên sinh viên 
+
+        public void updateName(String newName) {
+            this.name = newName;
+        }
     }
 
-    public String getStudentId() { return studentId; }
-    public String getName() { return name; }
-    public int getAge() { return age; }
-    public void setAge(int age) { this.age = age; }
-}
 }
